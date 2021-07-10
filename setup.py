41f1c75ea5ce3fa0abd8d8b3c2b2a79126fa4b20
sdamrich <sebastian.damrich@gmx.de>
2021-07-10 15:32:48 +0200

    prune setup.py to make it installable

--- setup.py
+++ setup.py
@@ -6,17 +6,6 @@ install_deps = ['numpy', 'scipy', 'natsort',
                 'torch>=1.6',
                 'opencv-python-headless']
 
-try:
-    import torch
-    a = torch.ones(2, 3)
-    version = int(torch.__version__[2])
-    if version >= 6:
-        install_deps.remove('torch')
-except:
-    pass
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
 setup(
     name="cellpose",
@@ -24,39 +13,11 @@ setup(
     author="Marius Pachitariu and Carsen Stringer",
     author_email="stringerc@janelia.hhmi.org",
     description="anatomical segmentation algorithm",
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MouseLand/cellpose",
-    setup_requires=[
-      'pytest-runner',
-      'setuptools_scm',
-    ],
     packages=setuptools.find_packages(),
-    use_scm_version=True,
     install_requires = install_deps,
-    tests_require=[
-      'pytest'
-    ],
-    extras_require = {
-      'docs': [
-        'sphinx>=3.0',
-        'sphinxcontrib-apidoc',
-        'sphinx_rtd_theme',
-      ],
-      'gui': [
-        'pyqtgraph==0.11.0rc0', 
-        'pyqt5', 
-        'pyqt5.sip',
-        'google-cloud-storage'
-        ]
-    },
-    include_package_data=True,
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-    ),
-     entry_points = {
+    entry_points = {
         'console_scripts': [
           'cellpose = cellpose.__main__:main']
      }
